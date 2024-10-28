e392b2289af80cff75714206be6e5df85d1ce943
D1Sorrow <61718663+D-Sorrow@users.noreply.github.com>
2024-10-28 10:06:31 -0500

    feat: Project start
    Project start

---
+++ src/main/java/com/esports_tournament/app/EsportsTournamentApplication.java
@@ -0,0 +1,13 @@
+package com.esports_tournament.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsportsTournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsportsTournamentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esports_tournament/app/EsportsTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esports_tournament.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsportsTournamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
